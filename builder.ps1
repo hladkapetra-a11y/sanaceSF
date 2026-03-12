0357d21fb292ad6e1ba9307f656cbfd33589a66e
Petra <hladka.petra@gmail.com>
2026-03-12 21:47:27 +0100

    verze07

---
+++ builder.ps1
@@ -0,0 +1,25 @@
+﻿$utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+$files = Get-ChildItem -Path . -Filter *.html | Where-Object { $_.Name -ne 'todo.html' }
+foreach ($file in $files) {
+    $content = [System.IO.File]::ReadAllText($file.FullName, $utf8NoBom)
+    
+    $content = [regex]::Replace($content, '<a href="kontakt\.html" class="([^"]*)btn-lg">Poptat (čištění|impregnaci|vyklízení)</a>', '<a href="https://sanacesf.cz/kontakt" class="$1btn-lg">Poptat $2</a>')
+    $content = [regex]::Replace($content, '<a href="kontakt\.html" class="([^"]*)btn-lg">Potřebuji diskrétní pomoc</a>', '<a href="https://sanacesf.cz/kontakt" class="$1btn-lg">Potřebuji diskrétní pomoc</a>')
+
+    if ($file.Name -eq 'kontakt.html') {
+        $content = $content.Replace('.hero-split { width:100%; background:#ffffff; overflow: hidden; }', '.hero-split { width:100%; background:#ffffff; overflow: hidden; padding: 0 !important; }')
+    }
+
+    if ($file.Name -in @('extremni-vyklizeni-a-cisteni.html', 'impregnace-strech-a-fasad.html')) {
+        $content = [regex]::Replace($content, '<div class="hero-image rounded-bg-image"[^>]+>', '<div class="hero-image">')
+    }
+
+    $content = [regex]::Replace($content, '(?i)\s*<link[^>]+rel="preconnect"[^>]+googleapis\.com[^>]*>', '')
+    $content = [regex]::Replace($content, '(?i)\s*<link[^>]+rel="preconnect"[^>]+gstatic\.com[^>]*>', '')
+    $fontLinkStr = '<link rel="preload" as="font" href="/fonts/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2" type="font/woff2" crossorigin>' + "`r`n    " + '<link rel="stylesheet" href="/css/montserrat.css">'
+    $content = [regex]::Replace($content, '(?i)<link[^>]+href="https://fonts\.googleapis\.com/css2\?family=Montserrat[^"]+"[^>]*rel="stylesheet">', $fontLinkStr)
+
+    $content = [regex]::Replace($content, '(?i)<script src="(/?)js/main\.js(\?v=[0-9]+)?"></script>', '<script src="$1js/main.js$2" defer></script>')
+
+    [System.IO.File]::WriteAllText($file.FullName, $content, $utf8NoBom)
+}
---
+++ fix_html.js
@@ -0,0 +1,37 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const dir = '.';
+const files = fs.readdirSync(dir).filter(f => f.endsWith('.html') && f !== 'todo.html');
+
+for (const file of files) {
+  const filePath = path.join(dir, file);
+  let content = fs.readFileSync(filePath, 'utf8');
+
+  // Fix Kontakt button links
+  content = content.replace(/<a href="kontakt\.html" class="btn btn-primary btn-lg">Poptat .*(čištění|impregnaci|vyklízení)<\/a>/g, (m) => m.replace('kontakt.html', 'https://sanacesf.cz/kontakt'));
+  content = content.replace(/<a href="kontakt\.html" class="btn btn-primary btn-lg">Potřebuji diskrétní pomoc<\/a>/g, (m) => m.replace('kontakt.html', 'https://sanacesf.cz/kontakt'));
+
+  // Fix Kontakt.html hero-split style
+  if (file === 'kontakt.html') {
+    content = content.replace('.hero-split { width:100%; background:#ffffff; overflow: hidden; }', '.hero-split { width:100%; background:#ffffff; overflow: hidden; padding: 0 !important; }');
+  }
+
+  // Fix Hero images to display properly
+  if (file === 'extremni-vyklizeni-a-cisteni.html' || file === 'impregnace-strech-a-fasad.html') {
+    content = content.replace(/<div class="hero-image rounded-bg-image"[^>]+>/g, '<div class="hero-image">');
+  }
+
+  // Replace Preconnects and Google Fonts
+  content = content.replace(/<link[^>]+rel="preconnect"[^>]+googleapis\.com[^>]*>\s*/g, '');
+  content = content.replace(/<link[^>]+rel="preconnect"[^>]+gstatic\.com[^>]*>\s*/g, '');
+  
+  const fontLinkStr = '<link rel="stylesheet" href="/css/montserrat.css">\n    <link rel="preload" as="font" href="/fonts/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2" type="font/woff2" crossorigin>\n    ';
+  content = content.replace(/<link[^>]+href="https:\/\/fonts\.googleapis\.com\/css2\?family=Montserrat[^"]+"[^>]*rel="stylesheet">\s*/g, fontLinkStr);
+
+  // Defer main.js
+  content = content.replace(/<script src="(\/?)js\/main\.js(\?v=[0-9]+)?"><\/script>/g, '<script src="/main.js" defer></script>');
+
+  fs.writeFileSync(filePath, content, 'utf8');
+}
+console.log('done');
